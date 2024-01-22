1704228ef53c1ec0f6df8da3df00a769aeac5067
karthiks8270 <karthikreddy8270@gmail.com>
2024-01-22 14:21:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class MyComponent extends Component{
+   constructor(props){
+    super(props);
+    this.state = {
+      count:0,
+    }
+   }
+   increment = () =>{
+    this.setState({count:this.state.count+1})
+   }
+   decrement = () =>{
+    this.setState({count:this.state.count-1})
+   }
+   restart =() =>{
+    this.setState({count:0})
+   }
+    render(){
+      return(
+        <div className='counter-app'>
+          <h2>Counter</h2>
+           <div>
+              <button className='btn btn-success counter-bt' onClick={this.increment}>+</button>
+              <button className='btn btn-danger  counter-bt' onClick={this.decrement}>-</button>
+              <button className='btn btn-primary counter-bt' onClick={this.restart}>Restart</button>
+           </div>
+           
+           <div></div>
+          <h1>{this.state.count}</h1>
+        </div>
+      )
+    }
 }
-
-export default App;
+export default MyComponent;
